afb304cdc005633f9633b96460880493e4680a6d
ajin0516 <dkwls7432@naver.com>
2022-11-10 18:36:37 +0900

    Dockerfile 생성

--- src/main/java/com/springbootmustache/bbs3/domain/dto/ArticleDto.java
+++ src/main/java/com/springbootmustache/bbs3/domain/dto/ArticleDto.java
@@ -3,9 +3,11 @@ package com.springbootmustache.bbs3.domain.dto;
 import com.springbootmustache.bbs3.domain.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class ArticleDto {
     private Long id;
     private String title;
