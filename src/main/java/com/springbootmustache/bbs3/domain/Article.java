852fe90350dbe28bb9b0e6c5f35d79e08bca8eae
ajin0516 <dkwls7432@naver.com>
2022-11-10 20:55:00 +0900

    Dockerfile 생성

--- src/main/java/com/springbootmustache/bbs3/domain/Article.java
+++ src/main/java/com/springbootmustache/bbs3/domain/Article.java
@@ -3,7 +3,6 @@ package com.springbootmustache.bbs3.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.persistence.*;
 
