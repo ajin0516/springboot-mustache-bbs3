ef775bfc215bdeafea87d0e25ce936ebc74a5e37
ajin0516 <dkwls7432@naver.com>
2022-11-14 15:22:54 +0900

    [Spring] Repository 메서드 생성 및 Test

--- src/main/java/com/springbootmustache/bbs3/repository/HospitalRepository.java
+++ src/main/java/com/springbootmustache/bbs3/repository/HospitalRepository.java
@@ -1,7 +1,19 @@
 package com.springbootmustache.bbs3.repository;
 
-import com.springbootmustache.bbs3.domain.dto.Hospital;
+import com.springbootmustache.bbs3.domain.entity.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HospitalRepository extends JpaRepository<Hospital, Integer> {
+
+    List<Hospital> findByBusinessTypeNameIn(List<String> businessTypeName);
+    List<Hospital> findByRoadNameAddressContaining(String keyword); // 포함
+    List<Hospital> findByHospitalNameStartsWith(String keyword); // 시작
+    List<Hospital> findByHospitalNameEndsWith(String keyword); // 끝남
+
+    List<Hospital> findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(int var1, int var2); // 10~20개 사이의 병상수
+    List<Hospital> findByPatientRoomCountBetween(Integer startCount, Integer endCount); // 10~20개 사이의 병상수
+
+    List<Hospital> findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(int a, int b); // 내림차순 - 오름차순(asc)
 }
--- src/test/java/com/springbootmustache/bbs3/repository/HospitalRepositoryTest.java
+++ src/test/java/com/springbootmustache/bbs3/repository/HospitalRepositoryTest.java
@@ -1,14 +1,12 @@
 package com.springbootmustache.bbs3.repository;
 
-import com.springbootmustache.bbs3.domain.dto.Hospital;
-import org.junit.jupiter.api.Assertions;
+import com.springbootmustache.bbs3.domain.entity.Hospital;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.persistence.Table;
-
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,6 +15,64 @@ class HospitalRepositoryTest {
 
     @Autowired
     HospitalRepository hospitalRepository;
+
+    @Test
+    @DisplayName("BusinessTypeName이 보건소 보건지소 보건지료소인 데이터가 잘 나오는지")
+    void BusinessTypeNameIn() {
+        List<String> inClues = new ArrayList<>();
+        inClues.add("보건소");
+        inClues.add("보건지소");
+        inClues.add("보건진료소");
+        List<Hospital> hospitals = hospitalRepository.findByBusinessTypeNameIn(inClues);
+        for (Hospital hospital : hospitals) {
+            System.out.println(hospital.getHospitalName());
+        }
+        System.out.println(hospitals.size());
+    }
+
+    void printHospitalNameAndAddress(List<Hospital> hospitals) {
+        for (var hospital : hospitals) {
+            System.out.printf("%s | %s | %s %f\n", hospital.getHospitalName(), hospital.getRoadNameAddress(), hospital.getPatientRoomCount(), hospital.getTotalAreaSize());
+        }
+
+        System.out.println(hospitals.size());
+    }
+
+    @Test
+    void containing() {
+        List<Hospital> hospitals = hospitalRepository.findByRoadNameAddressContaining("송파구");
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void startWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameStartsWith("경희"); // 가톨릭 서울 연세 결희
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void endsWith() {
+        List<Hospital> hospitals = hospitalRepository.findByHospitalNameEndsWith("병원"); //의원, 병원, 이비인후과, 치과
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void findByPatientRoomCountGreaterThanAndPatientRoomCount() {
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountGreaterThanAndPatientRoomCountLessThan(10, 20);
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void patientRoomCount(){
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetween(10, 20);
+        printHospitalNameAndAddress(hospitals);
+    }
+
+    @Test
+    void orderByPatientRoomCount() {
+        List<Hospital> hospitals = hospitalRepository.findByPatientRoomCountBetweenOrderByPatientRoomCountDesc(10, 20);
+        printHospitalNameAndAddress(hospitals);
+    }
 
     @Test
     void name(){
